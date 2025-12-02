5a1c1d182aeeddbf9dd79b2f88f9efa93991e6b7
tlaranje <tlaranje@student.42.porto.com>
2025-12-02 16:53:30 +0000

    :)

--- src/Algorithm/chunks.c
+++ src/Algorithm/chunks.c
@@ -6,7 +6,7 @@
 /*   By: tlaranje <tlaranje@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 16:17:10 by tlaranje          #+#    #+#             */
-/*   Updated: 2025/12/02 16:39:29 by tlaranje         ###   ########.fr       */
+/*   Updated: 2025/12/02 16:53:14 by tlaranje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,17 @@ int	chunks_sort(t_stack **stk_a, t_stack **stk_b, int chunk_size, int stk_size)
 
 	num_chunks = (stk_size + chunk_size - 1) / chunk_size;
 	ops = 0;
-	while (num_chunks--)
+	ft_printf("Num_chunks: %d\n\n", num_chunks);
+	while (num_chunks > 0)
 	{
-		//start_chunk =
+		start_chunk = (num_chunks - 1) * chunk_size;
+		end_chunk = start_chunk + chunk_size - 1;
+		if (end_chunk >= stk_size)
+			end_chunk = stk_size - 1;
+		ft_printf("Chunk %d\n", num_chunks);
+		ft_printf("Start_chunks: %d\n", start_chunk);
+		ft_printf("End_chunks: %d\n\n", end_chunk);
+		num_chunks--;
 	}
 	(void)stk_a;
 	(void)stk_b;
