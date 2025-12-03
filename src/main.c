7add7bdf631ebfc355108ea99d7b87e2a0f20fe2
tlaranje <tlaranje@student.42.porto.com>
2025-12-03 17:19:28 +0000

    :)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tlaranje <tlaranje@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 10:46:24 by tlaranje          #+#    #+#             */
-/*   Updated: 2025/12/03 09:47:47 by tlaranje         ###   ########.fr       */
+/*   Updated: 2025/12/03 10:11:03 by tlaranje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,28 @@
 
 void	print_stack(t_stack *stk, char *nome)
 {
-	ft_printf("+---------------+\n");
-	ft_printf("|    Stack %s    |\n", nome);
-	ft_printf("+---------------+\n");
-	printf("|   Size: %-3d   |\n", ft_stack_size(stk));
-	ft_printf("+---------------+\n");
-	ft_printf("| Index | Value |\n");
-	ft_printf("+---------------+\n");
+	ft_printf("+-------------------------------------+\n");
+	ft_printf("|              Stack %s                |\n", nome);
+	ft_printf("+-------------------------------------+\n");
+	printf("|              Size: %-3d              |\n", ft_stack_size(stk));
+	ft_printf("+-------------------------------------+\n");
+	ft_printf("| Index | Is_lis | Lis_length | Value |\n");
+	ft_printf("+-------------------------------------+\n");
 	while (stk)
 	{
-		printf("| %5d | %5d |\n", stk->index, stk->content);
+		printf("| %5d | %6d | %10d | %5d |\n",
+			stk->index, stk->is_lis, stk->lis_length, stk->content);
 		stk = stk->next;
 	}
-	ft_printf("+---------------+\n\n");
+	ft_printf("+-------------------------------------+\n\n");
 }
 
 static int	ft_sort(int ar, char *av[], t_stack **stk_a, t_stack **stk_b)
 {
-	int	stk_size;
-
 	if (ar < 2)
 		ft_error("Error");
 	if (!ft_check_args(ar, av, stk_a))
 		ft_error("Error");
-	stk_size = ft_stack_size(*stk_a);
 	add_index(stk_a);
 	if (ft_stack_size(*stk_a) == 2)
 		return (sort_2(stk_a));
@@ -48,8 +46,9 @@ static int	ft_sort(int ar, char *av[], t_stack **stk_a, t_stack **stk_b)
 	else if (ft_stack_size(*stk_a) == 5)
 		return (sort_5(stk_a, stk_b));
 	else if (ft_stack_size(*stk_a) > 5)
-		return (chunks_sort(stk_a, stk_b, get_chunk_size(stk_size), stk_size));
-		//return (radix_sort(stk_a, stk_b, stk_size));
+		//return (chunks_sort(stk_a, stk_b, get_chunk_size(stk_size), stk_size));
+		return (radix_sort(stk_a, stk_b, stk_size));
+
 	return (1);
 }
 
@@ -63,9 +62,9 @@ int	main(int ar, char *av[])
 	stk_b = NULL;
 	ops = 0;
 	ops += ft_sort(ar, av, &stk_a, &stk_b);
+	printf("\nOperations: %d\n\n", ops);
 	print_stack(stk_a, "A");
 	print_stack(stk_b, "B");
-	printf("\nOperations: %d\n\n", ops);
 	ft_stack_clear(&stk_a);
 	ft_stack_clear(&stk_b);
 	return (0);
